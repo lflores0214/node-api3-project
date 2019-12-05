fabc8c16058ac2045f178c98734617e3e8ff6952
Luis Floresgit config --global user.name Luis <Lflores02142@gmail.com>
2019-12-05 12:55:51 -0700

    added .env file

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ server.use("/api/users", userRouter)
 server.use("/api/posts", postRouter)
 
 server.get("/", (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+  const message = process.env.MSG || "hello world"
+  res.send(`${message}`);
 });
 
 //custom middleware
